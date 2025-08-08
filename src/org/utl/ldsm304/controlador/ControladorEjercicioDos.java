163d1dd244916a93de8b3af01f3c8cfaeca32ab7
Keviiin-999 <yahirkevin206@gmail.com>
2025-08-08 00:12:51 -0600

    Cree los metodos del ejercicio dos

--- src/org/utl/ldsm304/controlador/ControladorEjercicioDos.java
+++ src/org/utl/ldsm304/controlador/ControladorEjercicioDos.java
@@ -9,5 +9,41 @@ package org.utl.ldsm304.controlador;
  * @author uriel
  */
 public class ControladorEjercicioDos {
+    public String[] obtenerCadenasConO(String[][] arreglo) {
+        String resultado = "";
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                if (arreglo[i][j].toLowerCase().startsWith("o")) {
+                    resultado += arreglo[i][j] + ",";
+                }
+            }
+        }
+        return resultado.isEmpty() ? new String[]{} : resultado.split(",");
+    }
     
+    public String[] obtenerCadenasConTodasVocales(String[][] arreglo) {
+        String resultado = "";
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                String palabra = arreglo[i][j].toLowerCase();
+                if (palabra.contains("a") && palabra.contains("e") && palabra.contains("i") 
+                        && palabra.contains("o") && palabra.contains("u")) {
+                    resultado += arreglo[i][j] + ",";
+                }
+            }
+        }
+        return resultado.isEmpty() ? new String[]{} : resultado.split(",");
+    }
+    
+    public String[] obtenerCadenasConSa(String[][] arreglo) {
+        String resultado = "";
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                if (arreglo[i][j].toLowerCase().contains("sa")) {
+                    resultado += arreglo[i][j] + ",";
+                }
+            }
+        }
+        return resultado.isEmpty() ? new String[]{} : resultado.split(",");
+    }
 }
